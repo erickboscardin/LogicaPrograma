cf9ae2687b7ad7fe65797ed1ef5e39a241173d69
Erick Brayan Boscardim Barbosa <erickbosca@gmail.com>
2023-03-09 14:25:39 -0400

    Teste 01

---
+++ ProjetoTeste/ProjetoTeste/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTeste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("é o braya");
+            Console.ReadKey();
+        }
+    }
+}
